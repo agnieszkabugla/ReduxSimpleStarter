7e7bb77d510edc129d47f6d9a1c358700b3672c0
agnieszkabugla <agnieszka.bugla3@gmail.com>
2017-10-06 09:32:05 +0200

    final version of simple youtube clone

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import _ from 'lodash'; 
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'; 
+import SearchBar from './components/search_bar'; 
+import VideoList from './components/video_list'; 
+import VideoDetail from './components/video_detail'; 
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDJ8OHnPz8Bcfwu8EW5Q1ryHohBi_kxGHg'; 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component. this component should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         }; 
+
+         this.videoSearch('surfboards'); 
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+            //this.setState({ videos: videos }) === this.setState({ videos })
+            // above versions are ES6 / both are correct
+            // it only works when the key and the proparty are the same variable name 
+         });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 400); 
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/> 
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+                {/* passing props videos to VideoList - pasing data from parent to child */}
+            </div>
+            );
+    }
+}
+
+// take this component's generated HTML and put it one the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container')); 
